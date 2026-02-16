9146762decffb4859578fa87ddbafa16c25097b4
abdullayevv7 <abdullayev.axv@gmail.com>
2026-02-16 16:10:27 +0500

    hm

--- 16.02.2026.py
+++ 16.02.2026.py
@@ -14,6 +14,7 @@
 
 
 
+# #Polimorfizm
 class Transport:
     def __init__(self, rang):
         self.rang = rang
@@ -33,5 +34,43 @@ class Velosiped(Transport):
 mashina = Mashina("qizil")
 velosiped = Velosiped("yashil")
 
-mashina.harakatlan()    # "qizil mashina yo‘lda g‘ildirab ketyapti!"
-velosiped.harakatlan()  # "yashil velosiped pedalsiz ham yuradi!"
+mashina.harakatlan()
+velosiped.harakatlan()
+
+
+
+# #Inkapsulyatsiya
+class Mashina:
+    def __init__(self, rang, model):
+        self.__rang = rang
+        self.__model = model
+        self.__tezlik = 0
+
+    def get_rang(self):
+        return self.__rang
+
+    def set_rang(self, yangi_rang):
+        self.__rang = yangi_rang
+
+    def get_model(self):
+        return self.__model
+
+    def set_model(self, yangi_model):
+        self.__model = yangi_model
+
+    def get_tezlik(self):
+        return self.__tezlik
+
+    def set_tezlik(self, yangi_tezlik):
+        if yangi_tezlik >= 0:
+            self.__tezlik = yangi_tezlik
+        else:
+            print("Tezlik manfiy bo‘lishi mumkin emas!")
+
+mening_mashinam = Mashina("qizil", "Toyota")
+print(mening_mashinam.get_rang())
+mening_mashinam.set_rang("ko‘k")
+print(mening_mashinam.get_rang())
+mening_mashinam.set_tezlik(30)
+print(mening_mashinam.get_tezlik())
+mening_mashinam.set_tezlik(-10)
